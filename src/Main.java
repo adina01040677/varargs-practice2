3be038befc3761ef4482e93b4d1eb22065c90672
адина <adina.atakulova04@gmail.om>
2023-03-25 21:17:37 +0300

    String a = "  I like java!!!";         System.out.println(a.charAt(a.length()-1));          System.out.println(a.startsWith("  I like"));          System.out.println(a.endsWith( "!!!"));         System.out.println(a.contains("java"));         System.out.println(a.replace("a","o"));         String word = a.substring(9,13);         System.out.println(word.toUpperCase(Locale.ROOT));         System.out.println(word.toLowerCase(Locale.ROOT));         System.out.println(a.length());         System.out.println(a.trim());      }     }

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // varargs(new int[]{22, 19, 17}, "Nurayiym", "Adina", "Bayastan");
+//  System.out.println(a.endsWith("na")); Возвращает boolean значение. Проверяет наличие вами данной строки в конце
+//  System.out.println(a.startsWith("Mal")); Тоже возвращает boolean значение. Проверяет наличие данной вами строки в начале
+   //     System.out.println(a.indexOf('a')); // Возвращает индекс вам данного символа
+
+
+//    static void varargs( int[] age, String... varargs) {
+//        for (int i = 0; i < age.length; i++) {
+//            System.out.println(varargs[i] + " "+age[i]);
+//        }
+     String a = "  I like java!!!";
+        System.out.println(a.charAt(a.length()-1));
+         System.out.println(a.startsWith("  I like"));
+         System.out.println(a.endsWith( "!!!"));
+        System.out.println(a.contains("java"));
+        System.out.println(a.replace("a","o"));
+        String word = a.substring(9,13);
+        System.out.println(word.toUpperCase(Locale.ROOT));
+        System.out.println(word.toLowerCase(Locale.ROOT));
+        System.out.println(a.length());
+        System.out.println(a.trim());
+     }
     }
-}
+
+
